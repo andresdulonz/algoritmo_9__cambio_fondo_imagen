bab917e13bf3df3a82963d0966fcc804ea8702b8
andresdulonz <77663135+andresdulonz@users.noreply.github.com>
2021-09-28 17:16:07 -0500

    Cambio de fondo de imágenes
    Cambio de fondo de imágenes

---
+++ algoritmo_9__cambio_fondo_imagen/00_mascara_de_imagen.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread('Captura.PNG')
+
+im_fondo = cv2.imread('via_lactea_0.jpg')
+h,w,_ = img.shape
+im_fondo= cv2.resize(im_fondo, (w,h))
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+mask_i = cv2.inRange(hsv,(69,0,0),(69,255,255))
+mask = cv2.bitwise_not(mask_i)
+img_2 = cv2.bitwise_and(img, img, mask=mask)
+
+im3= cv2.bitwise_and(im_fondo, im_fondo, mask=mask_i)
+img_4= cv2.bitwise_or(im3, img_2)
+
+cv2.imshow('Imagen nuevo fondo',img_4)
+cv2.waitKey(0)
---
+++ algoritmo_9__cambio_fondo_imagen/04_ejercicio_2_mascara_3.py
@@ -0,0 +1,18 @@
+import cv2
+
+img = cv2.imread('proyector.jpg')
+
+im_fondo = cv2.imread('torre_2_3.jpg')
+h,w,_ = img.shape
+im_fondo= cv2.resize(im_fondo, (w,h))
+
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+mask_i = cv2.inRange(hsv,(30,50,50),(120,255,255))
+mask = cv2.bitwise_not(mask_i)
+img_2 = cv2.bitwise_and(img, img, mask=mask)
+
+im3= cv2.bitwise_and(im_fondo, im_fondo, mask=mask_i)
+img_4= cv2.bitwise_or(im3, img_2)
+
+cv2.imshow('Imagen nuevo fondo',img_4)
+cv2.waitKey(0)
